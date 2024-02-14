bd07155daf1e2f3192ab0340a06a510122fe0e86
dodo920306 <dodo920306@gmail.com>
2024-02-14 10:43:12 +0800

    Create 2149.c
    This is the solution for the leetcode daily question on 2024/2/14.

---
+++ 2149.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* rearrangeArray(int* nums, int numsSize, int* returnSize);
+
+int* rearrangeArray(int* nums, int numsSize, int* returnSize)
+{
+    int *res = (int *)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+    if (!res) {
+        perror("Error");
+        return NULL;
+    }
+
+    for (int i = 0, *pos = &res[0], *neg = &res[1], **indir; i < numsSize; i++) {
+        indir = (nums[i] >> ((sizeof(int) << 3) - 1)) ? &neg : &pos;
+        **indir = nums[i];
+        (*indir) += 2;
+    }
+    return res;
+}
+
+int main()
+{
+    int nums[6] = {3, 1, -2, -5, 2, -4}, returnSize, *res = rearrangeArray(nums, 6, &returnSize);
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", res[i]);
+    }
+    free(res);
+    return 0;
+}
