415355064e1a939c4b33e94440a9bc923b0fff87
dodo920306 <dodo920306@gmail.com>
2024-02-17 14:58:01 +0800

    Update `2.c` with a soultion without malloc()

--- 2.c
+++ 2.c
@@ -6,51 +6,107 @@ struct ListNode {
     struct ListNode *next;
 };
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2);
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+/* a no malloc() solution */
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
     int carry = 0;
-    struct ListNode *sum = (struct ListNode *)malloc(sizeof(struct ListNode)), *head = sum;
-    sum->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-    while (l1 && l2) {
-        sum = sum->next;
-        sum->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        sum->val = l1->val + l2->val + carry;
-        carry = sum->val / 10;
-        sum->val %= 10;
+    struct ListNode *head = l1;
+    while (l1->next && l2->next) {
+        l1->val += l2->val + carry;
+        carry = l1->val / 10;
+        l1->val %= 10;
         l1 = l1->next;
         l2 = l2->next;
     }
-    while (l1) {
-        sum = sum->next;
-        sum->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        sum->val = l1->val + carry;
-        carry = sum->val / 10;
-        sum->val %= 10;
+
+    l1->val += l2->val + carry;
+    carry = l1->val / 10;
+    l1->val %= 10;
+
+    /* use the original list space to store the result */ 
+    if (l2->next)   l1->next = l2->next;
+
+    if (l1->next) {
         l1 = l1->next;
+
+        while (l1->next) {
+            l1->val += carry;
+            carry = l1->val / 10;
+            l1->val %= 10;
+            l1 = l1->next;
+        }
+
+        l1->val += carry;
+        carry = l1->val / 10;
+        l1->val %= 10;
     }
-    while (l2) {
-        sum = sum->next;
-        sum->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        sum->val = l2->val + carry;
-        carry = sum->val / 10;
-        sum->val %= 10;
-        l2 = l2->next;
-    }
+
+    /* l2 must haven't been used */ 
     if (carry) {
-        sum = sum->next;
-        sum->val = carry;
+        l1->next = l2;
+        l1 = l1->next;
+        l1->val = carry;
     }
-    else
-        free(sum->next);
-    sum->next = NULL;
-    sum = head->next;
-    free(head);
-    return sum;
+
+    l1->next = NULL;
+    return head;
 }
 
 int main()
 {
+    struct ListNode *l1 = (struct ListNode *)malloc(sizeof(struct ListNode)), *l2 = (struct ListNode *)malloc(sizeof(struct ListNode)), *res, *iter;
+    if (!l1 || !l2) {
+        perror("Error");
+        return -1;
+    }
+    l1->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (!(l1->next)) {
+        perror("Error");
+        return -1;
+    }
+    l1->val = 2;
+    l1->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (!(l1->next->next)) {
+        perror("Error");
+        return -1;
+    }
+    l1->next->val = 4;
+    l1->next->next->next = NULL;
+    l1->next->next->val = 3;
+    l2->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (!(l2->next)) {
+        perror("Error");
+        return -1;
+    }
+    l2->val = 5;
+    l2->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (!(l2->next->next)) {
+        perror("Error");
+        return -1;
+    }
+    l2->next->val = 6;
+    l2->next->next->next = NULL;
+    l2->next->next->val = 4;
+    res = addTwoNumbers(l1, l2);
+    while (res) {
+        printf("%d ", res->val);
+        res = res->next;
+    }
+    /**
+     * No matter what we do to l1 and l2 in the function, 
+     * the values they have here won't change, so we can free them.
+     */
+    while (l1) {
+        iter = l1->next;
+        free(l1);
+        l1 = iter;
+    }
+    while (l2) {
+        iter = l2->next;
+        free(l2);
+        l2 = iter;
+    }
     return 0;
 }
