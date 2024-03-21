3b1767b6714735127d8aceb43d03cf4c3fc732f7
dodo920306 <dodo920306@gmail.com>
2024-03-21 09:03:10 +0800

    Create `206.c`
    Solution to "Reverse Linked List".

---
+++ 206.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *reverse(struct ListNode *prev, struct ListNode *head);
+struct ListNode *reverseList(struct ListNode *head);
+
+struct ListNode *reverse(struct ListNode *prev, struct ListNode *head)
+{
+    if (!head)  return prev;
+    struct ListNode *next = head->next;
+    head->next = prev;
+    return reverse(head, next);
+}
+
+struct ListNode *reverseList(struct ListNode *head)
+{
+    return reverse(NULL, head);
+}
+
+int main()
+{
+    struct ListNode node5 = {5, NULL}, node4 = {4, &node5}, node3 = {3, &node4}, node2 = {2, &node3}, node1 = {1, &node2}, *head = reverseList(&node1);
+    while (head) {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
+    return 0;
+}
