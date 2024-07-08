e02a79a1749402ae86cc4d14b4c0595f4ca4ad37
dodo920306 <dodo920306@gmail.com>
2024-07-08 13:14:27 +0800

    Create `1823.c`
    Solution to "Find the Winner of the Circular Game".

---
+++ 1823.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int findTheWinner(int n, int k);
+
+int findTheWinner(int n, int k)
+{
+    /**
+     * If there is only one node, it's the winner.
+     *
+     * If not, the problem shrink into the same problem with n - 1 nodes.
+     * The only difference there are the position of the beginning, from 1 to k + 1,
+     * so we add k every recursion.
+     *
+     * Finally, since it's a circular list, all operations have to be done
+     * under modulo n.
+     * To do this, we have to minus 1 from the previous result first.
+     * Then add it back after the modulo.
+     */
+    return n - 1 ? (findTheWinner(n - 1, k) + k - 1) % n + 1 : 1;
+}
+
+int main()
+{
+    printf("%d\n", findTheWinner(5, 2));
+    return 0;
+}
