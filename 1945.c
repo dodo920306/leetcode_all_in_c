18816bb7aa99c39f77ecaac6d8b8d38f3381ba91
dodo920306 <dodo920306@gmail.com>
2024-10-06 12:54:05 +0800

    Create `1945.c`
    Solution to "Sum of Digits of String After Convert".

---
+++ 1945.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int intConverter(int k);
+int charConverter(char c);
+int stringConverter(char *s);
+int getLucky(char *s, int k);
+
+int intConverter(int k)
+{
+    int res = 0;
+    while (k) {
+        res += k % 10;
+        k /= 10;
+    }
+    return res;
+}
+
+int charConverter(char c)
+{
+    return intConverter(c - '`');
+}
+
+int stringConverter(char *s)
+{
+    int res = 0;
+    for (int i = 0, length = strlen(s); i < length; i++)
+        res += charConverter(s[i]);
+    return res;
+}
+
+int getLucky(char *s, int k)
+{
+    int res = stringConverter(s);
+    k--;
+    while (k--)
+        res = intConverter(res);
+    return res;
+}
+
+int main()
+{
+    printf("%d\n", getLucky("leetcode", 2));
+    return 0;
+}
