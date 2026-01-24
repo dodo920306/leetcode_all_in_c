0fd88d4447485571615423f79ee4a1cd3372b4d8
dodo920306 <dodo920306@gmail.com>
2026-01-24 18:42:46 +0800

    Create `3315.c`
    Solution to "Construct the Minimum Bitwise Array II".
    
    Signed-off-by: dodo920306 <dodo920306@gmail.com>

---
+++ 3315.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int* minBitwiseArray(int* nums, int numsSize, int* returnSize);
+int minBitwiseNum(int num);
+
+int main()
+{
+    int nums[] = {2, 3, 5, 7};
+    int returnSize = 0;
+    int* res = minBitwiseArray(nums, 4, &returnSize);
+    printf("[");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d", res[i]);
+        if (i + 1 != returnSize) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    return 0;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* minBitwiseArray(int* nums, int numsSize, int* returnSize)
+{
+    for (int i = 0; i < numsSize; i++) {
+        nums[i] = minBitwiseNum(nums[i]);
+    }
+    *returnSize = numsSize;
+    return nums;
+}
+
+int minBitwiseNum(int num)
+{
+    if (num == 2) {
+        return -1;
+    }
+    if (!(num & (num - 1))) {
+        return num >> 1;
+    }
+    int bit = 1;
+    for (; num & (bit << 1); bit <<= 1);
+    /**
+     * if num == 11 (0b1011),
+     * bit = 2 (0b10),
+     * so num - bit = 0b1001, and
+     * 0b1001 & (0b1001 + 0b1)
+     * = 0b1001 & 0b1010
+     * = 0b1011 == 11
+     */
+    return num - bit;
+}
